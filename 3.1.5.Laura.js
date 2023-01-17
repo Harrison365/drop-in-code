d0da5f52623a06d3dcc4b40eb0b831e1fea47c29
Harrison Royds <harrisonroyds@harrisons-mbp.home>
2023-01-17 16:44:43 +0000

    17/1/23

---
+++ 3.1.5.Laura.js
@@ -0,0 +1,16 @@
+const veg = [
+  { name: "Parsnip", type: "root", quantity: 4 },
+  { name: "Broccoli", type: "brassica", quantity: 1 },
+  { name: "Carrot", type: "root", quantity: 5 },
+  { name: "Onion", type: "bulb", quantity: 3 },
+  { name: "Chard", type: "leaf", quantity: 3 },
+  { name: "Runner Beans", type: "legume", quantity: 8 },
+];
+
+//create an array of names for index 2 and 5
+//we want ["Carrot" and "Runner Beans"]
+const names = [];
+
+console.log(names);
+
+//All the names???
---
+++ 3.1.6.Jai.js
@@ -0,0 +1,7 @@
+let obj1 = { cake: 2, iceCream: 7 };
+let obj2 = { plates: 2, cups: 1, forks: 1, partyHats: 4 };
+
+function orderSupplies(obj, num) {}
+
+console.log(orderSupplies(obj1, 2));
+console.log(orderSupplies(obj2, 20));
