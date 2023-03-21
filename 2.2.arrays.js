99bb11a0c2945664d696dbe9f4437b42e3957a32
Harrison Royds <harrisonroyds@Harrisons-MacBook-Pro.local>
2023-03-21 17:55:43 +0000

    star wars array updated

--- 2.2.arrays.js
+++ 2.2.arrays.js
@@ -2,13 +2,13 @@ const starWarsArray = [
   "Jabba the Hutt",
   "Jar Jar Binks",
   ["Han Solo", "Princess Leia"],
-  ["Luke", ["Obi Wan", "Yoda", ["R2-D2", "C3-PO"]]],
+  ["Luke", ["Obi Wan", "Yoda", "R2-D2", "C3-PO"]],
 ];
 
 let jarJar = "?";
 let princess = "?";
 let goldRobot = "?";
 
-// console.log(jarJar, princess, goldRobot);
+console.log(jarJar, princess, goldRobot);
 
 //remove and save C3-PO as a variable
