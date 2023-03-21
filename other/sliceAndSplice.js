1a37eb6f3e92000ee5dc827a14635cc920d9630d
Harrison Royds <harrisonroyds@Harrisons-MacBook-Pro.local>
2023-03-21 12:55:50 +0000

    kata 20

--- other/sliceAndSplice.js
+++ other/sliceAndSplice.js
@@ -5,12 +5,12 @@ let arr = [0, 1, 2, 3, 4, 5];
 // console.log(str.slice(1, 8));
 // console.log(arr.slice(1, 4));
 // console.log(str.slice(1));
-// console.log(str.slice(-1));
+//console.log(str.slice(-1));
 // console.log(...arr.slice(1, 4));
 
 let fruits = ["apple", "banana", "mango", "orange", "kiwi"];
 
 //start, delete count, items to add...
-let removed = fruits.splice(2, 3, "hello", "yo");
+let removed = fruits.splice(1, 2, "hello", "yo");
 console.log(fruits);
 console.log(removed);
