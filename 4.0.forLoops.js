197b0fcaad3b8f33d55fecc7adf005788bd3d4c4
Harrison Royds <harrisonroyds@Harrisons-MacBook-Pro.local>
2023-04-13 19:38:40 +0100

    small edit

--- 4.0.forLoops.js
+++ 4.0.forLoops.js
@@ -1,4 +1,4 @@
-const array = [0, 1, 1, 8, 9, 9, 9, 8, 8, 1, 9, 9, 9, 1, 1, 9, 7, 2, 5, 3];
+const numbers = [5, 4, 1, 8, 9, 3, 9, 8, 2, 1, 9, 7, 9, 1, 6, 2, 7, 2, 5, 3];
 const fruits = ["apple", "orange", "banana", "kiwi"];
 //console.log every number
 //console.log every other number
