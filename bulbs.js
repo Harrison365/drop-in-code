0abe9f3f1721dacc39d07684b2194af95f1c44d9
Harrison Royds <harrisonroyds@Harrisons-MacBook-Pro.local>
2023-04-18 17:35:56 +0100

    ducks and bulbs

---
+++ bulbs.js
@@ -0,0 +1,20 @@
+/*Jim plans to buy new pack of assorted light bulbs.
+He has heard that light bulbs are the most efficient when their serial number
+is an odd number
+has exactly six digits
+He would appreciate you writing some code that figured out which serial numbers met the criteria.
+When his function runs, it will receive an array of serialNumbers - something like
+[32438, 34193, 149143, 4329429, 98537, 238791, 23492, 298342]
+You need to add code that will ensure Jim collects the serial numbers he wants to keep in the provided variable, efficientSerialNumbers.
+For the above array, this would mean that efficientSerialNumbers should end up containing
+[149143, 238791]*/
+
+//find the numbers that have 6 digits and are odd
+
+const numbers = [32438, 34193, 149143, 4329429, 98537, 238791, 23492, 298342];
+
+const newArr = [];
+
+//code here
+
+console.log(newArr);
---
+++ duck.js
@@ -0,0 +1,20 @@
+// Write a for loop which finds the index position of the duckling and assigns it to the provided variable 'ducklingHere'.
+
+const animals = [
+  "Alligator",
+  "Fox",
+  "Armadillo",
+  "Tiger",
+  "Duckling",
+  "Raccoon",
+  "Chicken",
+  "Sheep",
+  "Dog",
+  "Antelope",
+];
+
+let ducklingHere;
+
+// Start writing your code below
+
+console.log(ducklingHere);
