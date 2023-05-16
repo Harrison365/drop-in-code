46f2d5b5e2b9f08c6661e52e54407b891d7fb3f4
Harrison Royds <harrisonroyds@Harrisons-MacBook-Pro.local>
2023-05-16 13:59:33 +0100

    santhy

---
+++ santhy.js
@@ -0,0 +1,9 @@
+//Make each first letter upper case
+
+function jadenCase(string) {}
+
+// console.log(jadenCase("change the first letter please"));
+
+//code wars was for a method
+
+console.log("Harrison".slice(-2));
