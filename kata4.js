e2c65054f3b77a7ff8e972c9cce51996587dca57
Harrison Royds <harrisonroyds@yahoo.co.uk>
2023-07-26 10:56:05 +0100

    comments added

---
+++ kata4.js
@@ -0,0 +1,14 @@
+//Complete the function findFirstOdd, so that it takes an array of numbers and returns the first odd number.
+
+// If there are no odd numbers, the function should return undefined.
+
+// Examples:
+
+// findFirstOdd([1,4,5])
+// // returns 1
+
+// findFirstOdd([2,4,5])
+// // returns 5
+
+// findFirstOdd([2,4,6])
+// // returns undefined
