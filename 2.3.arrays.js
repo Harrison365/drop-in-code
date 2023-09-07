025084670c34ed5a36ba9b908fd8aeb8174e59c1
Harrison Royds <harrisonroyds@yahoo.co.uk>
2023-09-07 17:43:52 +0100

    mario alt to starwars added

---
+++ 2.3.arrays.js
@@ -0,0 +1,14 @@
+const nintendoArray = [
+  "Mario",
+  "Luigi",
+  ["Daisy", "Peach"],
+  ["Yoshi", ["Toad", "Warrio", "Waluigi", "Bowser"]],
+];
+
+let greenMan = "?";
+let princess = "?";
+let mushroom = "?";
+
+console.log(greenMan, princess, mushroom);
+
+// //remove and save Toad as a variable
