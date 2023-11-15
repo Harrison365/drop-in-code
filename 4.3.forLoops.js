eedb9dae8cce3b85e0947ca13644f8a32499a89b
Harrison Royds <harrisonroyds@yahoo.co.uk>
2023-11-15 12:19:14 +0000

    changed word in the array

--- 4.3.forLoops.js
+++ 4.3.forLoops.js
@@ -1,2 +1,2 @@
-const words = ["dog", "spanner", "Jurgen", "Klopp", "cat", "iPhone"];
+const words = ["dog", "winner", "Jurgen", "Klopp", "cat", "iPhone"];
 //Write a function which takes an array of strings and returns a count of the number of strings with more than 3 letters.
