a69dffb05e5e7d0737ba762d00867213ab4256d7
nc-harrison-r <harrison.royds@northcoders.com>
2024-09-05 15:02:16 +0100

    added kata 8

---
+++ 8kata.js
@@ -0,0 +1,14 @@
+// Is This a Palindrome?
+// Write a function that takes a string, and returns true if it is a palindrome, and false if it is not.
+
+// (A palindrome is a word that, when reversed, is exactly the same. e.g. "madam" or "racecar")
+
+// Examples:
+
+// isThisAPalindrome('yay');
+// // returns true
+
+// isThisAPalindrome('no');
+// // returns false
+
+function isThisAPalindrome(str) {}
